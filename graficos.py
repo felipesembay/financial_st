df4f562839a18ce079fbf7a6c1343e7cdd4d54c5
felipesembay <felipesembay91@gmail.com>
2024-01-01 21:19:21 -0300

    Alteração1

--- graficos.py
+++ graficos.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 from sqlalchemy import create_engine
-import plotly.express as px
 
 # Configuração de conexão com o banco de dados
 db_config = {
@@ -21,17 +20,22 @@ engine = create_engine(connection_string)
 # Execute uma consulta SQL
 receita_query = "SELECT SUM(Valor) as Total FROM receitas"
 despesa_query = "SELECT SUM(Valor) as Total FROM despesas"
-investimento_query = "SELECT SUM(custo_aquisicao) as Total FROM investimentos"
+investimento_compra_query = "SELECT SUM(custo_aquisicao) as Total FROM investimentos WHERE Status = 'Compra'"
+investimento_venda_query = "SELECT SUM(custo_aquisicao) as Total FROM investimentos WHERE Status = 'Venda'"
 
 receitas = pd.read_sql(receita_query, engine)
 despesas = pd.read_sql(despesa_query, engine)
-investimentos = pd.read_sql(investimento_query, engine)
+investimentos_compra = pd.read_sql(investimento_compra_query, engine)
+investimentos_venda = pd.read_sql(investimento_venda_query, engine)
 
 # Verifica se os DataFrames não estão vazios antes de tentar acessar os valores
-if not receitas.empty and not despesas.empty and not investimentos.empty:
+if not receitas.empty and not despesas.empty and not investimentos_compra.empty and not investimentos_venda.empty:
     total_receitas = receitas['Total'].iloc[0] if receitas['Total'].iloc[0] else 0
     total_despesas = despesas['Total'].iloc[0] if despesas['Total'].iloc[0] else 0
-    total_investimentos = investimentos['Total'].iloc[0] if investimentos['Total'].iloc[0] else 0
+    total_investimentos_compra = investimentos_compra['Total'].iloc[0] if investimentos_compra['Total'].iloc[0] else 0
+    total_investimentos_venda = investimentos_venda['Total'].iloc[0] if investimentos_venda['Total'].iloc[0] else 0
+
+    total_investimentos_liquido = total_investimentos_compra - total_investimentos_venda
 
     # Layout de colunas
     col1, col2, col3, col4 = st.columns(4)
@@ -42,9 +46,9 @@ if not receitas.empty and not despesas.empty and not investimentos.empty:
     with col2:
         st.metric(label="Total Despesas", value=f"R$ {total_despesas:.2f}")
     with col3:
-        st.metric(label="Total Investimentos", value=f"R$ {total_investimentos:.2f}")
+        st.metric(label="Total Investimentos", value=f"R$ {total_investimentos_liquido:.2f}")
     with col4:
-        saldo = total_receitas - total_despesas - total_investimentos
+        saldo = total_receitas - total_despesas - total_investimentos_liquido
         st.metric(label="Caixa", value=f"R$ {saldo:.2f}")
 else:
     st.error("Não foi possível recuperar os dados.")
